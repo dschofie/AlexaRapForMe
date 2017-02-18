795983cd479927bb275a389baec0bef29f689bc3
dschofie <dannyschofield28@gmail.com>
2017-02-18 11:51:05 -0500

    get song

--- ourCode.py
+++ ourCode.py
@@ -8,10 +8,9 @@ http://amzn.to/1LGWsLG
 """
 
 from __future__ import print_function
-from bs4 import BeautifulSoup
 import requests
-import re
-import sys
+from bs4 import BeautifulSoup
+
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -68,11 +67,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "rap":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
+        return rapping(intent, session)
     else:
         raise ValueError("Invalid intent")
 
@@ -88,77 +83,21 @@ def on_session_ended(session_ended_request, session):
 
 # --------------- Functions that control the skill's behavior ------------------
 
-
-def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
+def rapping(intent, session):
     session_attributes = {}
     should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = myfunc('Feeling better than some head')#intent['slots']['keyword']['value']) 
-        reprompt_text = ""
+    speech_output = myfunc(intent['slots']['keyword']['value']) 
+    reprompt_text = ""
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        "", speech_output, reprompt_text, should_end_session))
+
+
+
 
 def myfunc(keyword):
     path = get_path_from_search(keyword)
     return get_lyrics(path)
-    
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
 
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
 def get_path_from_search(search_param):
@@ -178,6 +117,7 @@ def get_lyrics(path):
     lyrics = html.find('lyrics').get_text().encode(sys.stdout.encoding, errors='replace')
     lyrics = re.sub('\[.*\]','',lyrics)
     return lyrics
+
 
 # --------------- Helpers that build all of the responses ----------------------
 
